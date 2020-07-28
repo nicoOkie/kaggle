d0a728b8c1fe7673a843b842b297aa18910a17e0
Nicolas Chapon <chapon.nicola@gmail.com>
2020-07-28 17:08:38 +0200

    cette fois, ca marche

--- split.py
+++ split.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('./data/train.csv')
-df.dropna()
+df.dropna(axis=0)
 
 y = df['prix']
 x = df.drop('prix', axis=1)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=69)
 
-y_test.to_csv('./data/test2.csv')
-y_test.to_csv('./test2-predictions.csv')
+y_test.to_csv('./data/test2.csv', index=False, header=None)
+y_test.to_csv('./test2-predictions.csv', index=False, header=None)
